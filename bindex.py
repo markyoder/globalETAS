db2cce064fc84dfcd1057ead86a480af0999c94c
mark.yoder@gmail.com <mark.yoder@gmail.com>
2015-06-22 12:09:23 -0700

    reorgainized a bit: typed Bindex2d as a dict -- aka, now inherits from dict. this basically replaces the syntax like B.items.{anything} to B.{anything}. the rest of the exiting code just handle bin sizes, etc.

--- bindex.py
+++ bindex.py
@@ -45,7 +45,7 @@ def bindex2d_sample():
 	return B3
 	
 
-class Bindex2D(object):
+class Bindex2D(dict):
 	
 	# for now, let's just start with a 2D bindex and we'll generalize later
 	def __init__(self, dx=1., dy=1., x0=0., y0=0., leaf_type=float):
@@ -61,12 +61,12 @@ class Bindex2D(object):
 		#
 		self.leaf_type=leaf_type
 		#
-		self.items = {}
+		#self.items = {}
 	#
-	def __iter__(self):
-		return self.items.__iter__
-	def __sizeof__(self):
-		return sum([len(rw) for rw in self.items.iteritems()])
+	#def __iter__(self):
+	#	return self.items.__iter__
+	#def __sizeof__(self):
+	#	return sum([len(rw) for rw in self.items.iteritems()])
 	#
 	def get_xbin(self, x, dx=None, x0=None, bin_0=0):
 		dx = (dx or self.dx)
@@ -106,7 +106,8 @@ class Bindex2D(object):
 		# get all the items in the bin that contains coordinates x,y
 		x0, y0 = self.get_xybins(x,y, r_type='tuple')
 		#
-		return self.items[x0][y0]
+		#return self.items[x0][y0]
+		return self[x0][y0]
 	#
 	#def add_item(self, x=None, y=None, z=None, unpack_z=False):
 	#
@@ -122,15 +123,22 @@ class Bindex2D(object):
 		x0 = get_bin(x, dx=self.dx, x0=self.x0, bin_0=0)
 		y0 = get_bin(y, dx=self.dy, x0=self.y0, bin_0=0)
 		#
-		if not self.items.has_key(x0): self.items[x0]={}
-		if not self.items[x0].has_key(y0):
-			self.items[x0][y0]=self.leaf_type()
+		#if not self.items.has_key(x0): self.items[x0]={}
+		#if not self.items[x0].has_key(y0):
+		#	self.items[x0][y0]=self.leaf_type()
+		
+		if not self.has_key(x0): self[x0]={}
+		if not self[x0].has_key(y0):
+			self[x0][y0]=self.leaf_type()
+			
 		#
 		#if unpack_z==True and hasattr(z, '__len__'):
 		#	self.items[x0][y0]+=z
 		#else:
 		#	self.items[x0][y0]+=[z]
-		self.items[x0][y0]+=z
+		
+		#self.items[x0][y0]+=z
+		self[x0][y0]+=z
 		#
 		return None
 	#
@@ -144,7 +152,8 @@ class Bindex2D(object):
 		#	r_list += [[x, bin2x(Y, self.dy, self.y0, self.bin_y_0), z] for Y,z in Y_rw.iteritems()] 
 		
 		#
-		return [[bin2x(X, self.dx, self.x0, self.bin_x_0), bin2x(Y, self.dy, self.y0, self.bin_y_0), z] for X,Y_rw in self.items.iteritems()  for Y,z in Y_rw.iteritems()]
+		#return [[bin2x(X, self.dx, self.x0, self.bin_x_0), bin2x(Y, self.dy, self.y0, self.bin_y_0), z] for X,Y_rw in self.items.iteritems()  for Y,z in Y_rw.iteritems()]
+		return [[bin2x(X, self.dx, self.x0, self.bin_x_0), bin2x(Y, self.dy, self.y0, self.bin_y_0), z] for X,Y_rw in self.iteritems()  for Y,z in Y_rw.iteritems()]
 	#
 	def to_array(self):
 		#
@@ -169,7 +178,8 @@ class Bindex2D(object):
 		# we will have to assume that all the values in the row are of the same type. for now, we'll just let that break upon exception.
 		z_type = float
 		do_break=False
-		for j,rw_j in enumerate(self.items.values()):
+		#for j,rw_j in enumerate(self.items.values()):
+		for j,rw_j in enumerate(self.values()):
 			#print "array-checking row: ", j
 			for k,rw_k in enumerate(rw_j.values()):
 				if len(rw_k)>0:
@@ -179,7 +189,8 @@ class Bindex2D(object):
 			if do_break:
 				#print "break index: ", j,k
 				break
-		z_type = type(self.items.values()[0].values()[0][0])
+		#z_type = type(self.items.values()[0].values()[0][0])\
+		z_type = type(self.values()[0].values()[0][0])
 		#print "z_type: ", z_type
 		z_col_names = ['z_%d' % j for j in xrange(max_z_len)]
 		#
