10253a3b3555c7f6cdc904946f6a234412e5b8fd
mark ydoer <mark.yoder@gmail.com>
2016-02-06 11:34:40 -0600

    synching with remote

---
+++ etas_auto.py
@@ -0,0 +1,55 @@
+'''
+# an implementation of etas_auto() for 1) globalETAS, and 2) in python3
+#
+'''
+
+import globalETAS
+import ANSStools as atp
+
+
+def etas_auto(lon_center=None, lat_center=None, d_lat_0=.25, d_lon_0=.5, dt_0=10, Lr_factor=4.0, mc=2.5, mc_0=4.5, gridsize=.1, to_dt=None, fnameroot='etas_auto', catlen=5.0*365.0, doplot=False, kmldir='kml_auto', **kwargs):
+	'''
+	# this is probably a major staple of future work in ETAS; when in doubt, start here and point it at a big earthquake.
+	#
+	# a starter script to auto-select some parameters for an ETAS run. in practice, give this script a center location (probably a mainshock epicenter).
+	# the script will find the largest earthquake in that region and scale up an ETAS parameter set accordingly.
+		# d_lat_0, d_lon_0, dt_0 are the starting catalog parameters (largest earthquake in d_lat_0 x d_lon_0 x dt_0 cube).
+	'''
+	#
+	#if to_dt == None: to_dt = dtm.datetime.now(pytz.timezone('UTC'))
+	to_dt = (to_dt or dtm.datetime.now(pytz.timezone('UTC')))
+	mc_0  = (mc_0 or mc)
+	#
+	if lon_center==None and lat_center==None:
+		# let's look for any large earthquake in the world. assume for this, mc
+		mc_0=6.0
+		lat_center = 0.
+		lon_center = 0.
+		d_lat_0 = 88.
+		d_lon_0 = -180.
+	#
+	# get a preliminary catalog:
+	cat_0 = atp.catfromANSS(lon=[lon_center-d_lon_0, lon_center+d_lon_0], lat=[lat_center - d_lat_0, lat_center+d_lat_0], minMag=mc_0, dates0=[to_dt-dtm.timedelta(days=dt_0), to_dt], fout=None, rec_array=True)
+	#
+	#biggest_earthquake = filter(lambda x: x['mag']==max(cat_0['mag']), cat_0)[0]
+	mainshock = {cat_0.dtype.names[j]:x for j,x in enumerate(filter(lambda x: x['mag']==max(cat_0['mag']), cat_0)[0])}
+	#
+	# now, get new map domain based on rupture length, etc.
+	L_r = .5*10.0**(.5*mainshock['mag'] - 1.76)
+	d_lat = Lr_factor*L_r/lon2km
+	d_lon = Lr_factor*L_r/(lon2km*math.cos(deg2rad*mainshock['lat']))
+	print("mainshock data: ", mainshock, L_r, d_lat, d_lon)
+	#
+	working_cat = atp.catfromANSS(lon=[mainshock['lon']-d_lon, mainshock['lon']+d_lon], lat=[mainshock['lat']-d_lat, mainshock['lat']+d_lat], minMag=mc, dates0=[to_dt-dtm.timedelta(days=catlen), to_dt], fout=None, rec_array=True)
+	#
+	print("biggest event(s): ", [rw for rw in working_cat if rw['mag']==max(working_cat['mag'])])
+	#
+	# now, do some ETAS:
+	# skip working_cat above, but parse lon, lat, etc. parameters similarly. pass those (and other) params to make_etas_fcfiles()
+	# looks like this: make_etas_fcfiles(root_prams=nepal_ETAS_prams, **kwargs), and:
+	# nepal_ETAS_prams = {'todt':None, 'gridsize':.1, 'contres':5, 'mc':4.5, 'kmldir':kmldir, 'catdir':kmldir, 'fnameroot':'nepal', 'catlen':5.0*365.0, 'doplot':False, 'lons':[nepal_epi_lon-nepal_dlon, nepal_epi_lon+nepal_dlon], 'lats':[nepal_epi_lat-nepal_dlat, nepal_epi_lat+nepal_dlat], 'bigquakes':None, 'bigmag':7.00, 'eqtheta':None, 'eqeps':None, 'fitfactor':5.0, 'cmfnum':0, 'fignum':1, 'contour_intervals':None}
+	root_prams = {'todt':None, 'gridsize':gridsize, 'contres':10, 'mc':mc, 'kmldir':kmldir, 'catdir':kmldir, 'fnameroot':fnameroot, 'catlen':catlen, 'doplot':False, 'lons':[mainshock['lon']-d_lon, mainshock['lon']+d_lon], 'lats':[mainshock['lat']-d_lat, mainshock['lat']+d_lat], 'bigquakes':None, 'bigmag':mainshock['mag']-1.5, 'eqtheta':None, 'eqeps':None, 'fitfactor':5.0, 'cmfnum':0, 'fignum':1, 'contour_intervals':None}
+	#root_prams = {}
+	print("now execute with root_prams: ", root_prams)
+	#my_kwargs = {}
+
