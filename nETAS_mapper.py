9331de25f3290f20a486f92536126b1e3582b456
Mark Yoder <mark.yoder@gmail.com>
2021-07-10 19:31:41 -0700

    fixed some buts in the plotting routines. set transforms to ax.projection for better internal consistency. have contour and boxy_contour plots working

--- nETAS_mapper.py
+++ nETAS_mapper.py
@@ -61,7 +61,8 @@ class nETAS_mapper():
         if lons_map is None: lons_map = self.lons
         if lats_map is None: lats_map = self.lats
         cntr = [numpy.mean(lons_map), numpy.mean(lats_map)]
-        self.lon0 = cntr[0]
+        #self.lon0 = cntr[0]
+        self.lon0 = 0.0
         #
         lon_line_spacing = (lon_line_spacing or 1.)
         lat_line_spacing = (lat_line_spacing or 1.)
@@ -93,16 +94,12 @@ class nETAS_mapper():
         #
         return ax
     #
-    def make_etas_contour_map(self, n_contours=None, fignum=0, fig_size=(6.,6.), contour_fig_file=None, contour_kml_file=None, kml_contours_bottom=0., kml_contours_top=1.0, alpha=.5, alpha_kml=.5, refresh_etas=False, map_resolution='i', map_projection='cyl', map_cmap=None, lat_interval=None, lon_interval=None, lats_map=None, lons_map=None, ax=None, do_colorbar=True, do_states=True, do_rivers=True, lake_color='blue', Z=None , **kwargs):
+    def make_etas_contour_map(self, n_contours=None, fignum=0, fig_size=(6.,6.), contour_fig_file=None, contour_kml_file=None, kml_contours_bottom=0., kml_contours_top=1.0, alpha=.5, alpha_kml=.5, refresh_etas=False, map_resolution='i', map_projection=None, map_cmap=None, lat_interval=None, lon_interval=None, lats_map=None, lons_map=None, ax=None, do_colorbar=True, do_states=True, do_rivers=True, lake_color='blue', Z=None , **kwargs):
         #
         ax = self.draw_map(**{k:v for k,v in locals().items() if not k in ('self', '__class')})
         if map_cmap is None: map_cmap = self.cmap_contours
         #
         n_contours = (n_contours or self.n_contours)
-        #if ax is None:
-        #    fg=plt.figure(fignum)
-        #    ax=fg.gca()
-        
         #
         fg=plt.gcf()
         #
@@ -112,7 +109,7 @@ class nETAS_mapper():
         if Z is None: Z = numpy.log10(self.lattice_sites)
         #
         etas_contours = ax.contourf(X,Y, Z, n_contours, zorder=8, alpha=alpha,\
-            transform=self.map_projection, cmap=map_cmap)
+            transform=ax.projection, cmap=map_cmap)
         # ax.colorbar() ??
         if do_colorbar:
             try:
@@ -125,23 +122,31 @@ class nETAS_mapper():
         return ax
         #
     #
-    def make_etas_boxy_map(self, n_contours=None, fignum=0, fig_size=(6.,6.), contour_fig_file=None, contour_kml_file=None, kml_contours_bottom=0., kml_contours_top=1.0, alpha=.6, alpha_kml=.5, refresh_etas=False, map_resolution='i', map_projection='cyl', map_cmap='jet'):
+    def make_etas_boxy_map(self, n_contours=None, fignum=0, fig_size=(6.,6.), contour_fig_file=None, contour_kml_file=None, kml_contours_bottom=0., kml_contours_top=1.0, alpha=.6, alpha_kml=.5, refresh_etas=False, map_resolution='i', map_projection=None, map_cmap='jet', ax=None):
         #
-        cm = self.draw_map(fignum=fignum, fig_size=fig_size, map_resolution=map_resolution, map_projection=map_projection)
+        ax = self.draw_map(**{k:v for k,v in locals().items() if not k in ('self', '__class')})
+        #
         c_map = plt.get_cmap(map_cmap)
         zs = numpy.log10(self.ETAS_array['z'])
         cNorm = mpl.colors.Normalize(vmin=min(zs), vmax=numpy.nanmax(zs))
         scalarMap = mpl.cm.ScalarMappable(norm=cNorm, cmap=c_map)
         #
         for rw in self.ETAS_array:
-            plt.fill_between(x=[rw['x']-self.d_lon/2., rw['x']+self.d_lon/2.], y1=[rw['y']-.5*self.d_lat, rw['y']-.5*self.d_lat], y2=[rw['y']+.5*self.d_lat, rw['y']+.5*self.d_lat], color=scalarMap.to_rgba(numpy.log10(rw['z'])))
+            #print('** rw: {}, {}, {}'.format(rw['x'], rw['y'], rw['z']) )
+            #ax.plot([rw['x'], rw['y']], marker='.', transform=self.map_projection)
+            ax.add_patch(mpl.patches.Rectangle(xy=[rw['x']-self.d_lon-self.lon0, rw['y']-self.d_lat], width=self.d_lon, height=self.d_lat,
+                                    facecolor=scalarMap.to_rgba(numpy.log10(rw['z'])),
+                                    alpha=0.8,
+                                    transform=ax.projection)
+                                    )
+#            ax.fill_between(x=[rw['x']-self.d_lon/2., rw['x']+self.d_lon/2.], y1=[rw['y']-.5*self.d_lat, rw['y']-.5*self.d_lat], y2=[rw['y']+.5*self.d_lat, rw['y']+.5*self.d_lat], color=scalarMap.to_rgba(numpy.log10(rw['z'])), transform=ax.projection, alpha=alpha, zorder=11 )
         #plt.colorbar()        # not sure how to make this work for non-contour plot...
         #
-        return cm
+        return ax
         #
     def plot_mainshock_and_aftershocks(self, m0=6.0, n_contours=25, mainshock=None, fignum=0, ax=None):
         #
-        map_etas = self.make_etas_contour_map(n_contours=n_contours, fignum=fignum, ax=ax)
+        #map_etas = self.make_etas_contour_map(n_contours=n_contours, fignum=fignum, ax=ax)
         if mainshock is None:
             mainshock = self.catalog[0]
             for rw in self.catalog:
@@ -152,13 +157,14 @@ class nETAS_mapper():
         #
         for eq in self.catalog:
             if eq['mag']<m0 or eq['event_date']<ms['event_date']: continue
+            x = eq['lon'] - self.lon0
+            y = eq['lat']
             if eq==ms:
-                x,y = map_etas(eq['lon'], eq['lat'])
-                ax.plot([x], [y], 'k*', zorder=7, ms=20, alpha=.8)
-                ax.plot([x], [y], 'r*', zorder=8, ms=18, label='mainshock', alpha=.8)
+                #
+                ax.plot([x], [y], 'k*', zorder=7, ms=20, alpha=.8, transform=ax.projection)
+                ax.plot([x], [y], 'r*', zorder=8, ms=18, label='mainshock', alpha=.8, transform=ax.projection)
             if eq['event_date']>eq['event_date']:
-                x,y = map_etas(eq['lon'], eq['lat'])
-                ax.plot([x], [y], 'o', zorder=7, ms=20, alpha=.8)
+                ax.plot([x], [y], 'o', zorder=7, ms=20, alpha=.8, transform=ax.projection)
         #
         #return plt.gca()
         return ax
